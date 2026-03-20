29bf90bdecc8fdb26ac2eb970e7931544da166c6
Shaik Awez <shaikawez.dev@gmail.com>
2026-03-20 09:00:43 +0000

    config routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
+import {
+  register,
+  login,
+  refreshToken,
+  logout,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+  googleLogin,
+} from "../controllers/auth.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const authRouter = Router();
 
-authRouter.route("/register").post();
-authRouter.route("/login").post();
+authRouter.route("/register").post(register);
+authRouter.route("/login").post(login);
+authRouter.route("/logout").post(verifyJWT, logout);
+authRouter.route("/refresh-token").post(refreshToken);
+authRouter.route("/google").post(googleLogin);
+authRouter.route("/reset-password").post(resetPassword);
+authRouter.route("/forgot-password").post(forgotPassword);
+authRouter.route("/verify-email/:token").get(verifyEmail);
 
 export { authRouter };
