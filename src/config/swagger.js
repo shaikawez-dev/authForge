d5ef2542167164417c7da9f8657396008ef84445
Shaik Awez <shaikawez.dev@gmail.com>
2026-03-28 04:24:42 +0000

    add url

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api/v1",
+        url: "https://authforge-vkjh.onrender.com",
       },
     ],
     components: {
